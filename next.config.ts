db1b73cc83f5f08a916764c2f22e8744314a18f2
tasha4kotova <tasha4kotova@yandex.ru>
2025-06-27 17:18:26 +0300

    fix: move pwa options into pwa object

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
