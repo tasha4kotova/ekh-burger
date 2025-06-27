61919fa3f6e0c52f3faf49a988beeeeb0aa265d0
tasha4kotova <tasha4kotova@yandex.ru>
2025-06-27 17:15:34 +0300

    fix: pass all options directly to withPWA

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,13 @@ import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default withPWA({
-  ...nextConfig,
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
+export default withPWA(nextConfig);
