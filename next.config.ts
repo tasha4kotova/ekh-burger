08cf4ece9a60367a7dd3c652ee690e3235dffae4
tasha4kotova <tasha4kotova@yandex.ru>
2025-06-27 17:40:13 +0300

    fix: move pwa options to root for next-pwa 5

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+
 const nextConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
