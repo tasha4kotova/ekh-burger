c5a731ca5f9990a9a48bb1f4e9ff8c626b356056
tasha4kotova <tasha4kotova@yandex.ru>
2025-06-27 17:45:36 +0300

    test: disable pwa for build check

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import withPWA from "next-pwa";
 
 const nextConfig = {
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
