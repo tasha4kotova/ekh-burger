56ef57971ef92a924207741de3594f0d695e1bea
tasha4kotova <tasha4kotova@yandex.ru>
2025-06-27 17:08:39 +0300

    fix: move eslint config out of pwa config

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,9 @@ const nextConfig: NextConfig = {
 };
 
 export default withPWA({
-  ...nextConfig,
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  ...nextConfig,
 });
